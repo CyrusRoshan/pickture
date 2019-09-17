22eaf573b7df2082be725d5370f49f8ead66154c
Cyrus Roshan <cyrusroshan123@gmail.com>
2019-09-16 22:38:31 -0700

    Add (buggy) movement logic.

--- ui/logic.go
+++ ui/logic.go
@@ -22,6 +22,10 @@ var stateInfo = struct {
 	files []files.File
 }{}
 
+func currentFile() files.File {
+	return stateInfo.files[0]
+}
+
 func RenderCurrentState(win *pixelgl.Window) {
 	// Get changes to previous state
 	input.CalculateKeyPressChanges(win)
@@ -31,7 +35,7 @@ func RenderCurrentState(win *pixelgl.Window) {
 
 	// Draw current state
 	win.Clear(colornames.Black)
-	DrawBackgroundImage(win, stateInfo.files[0])
+	DrawBackgroundImage(win, currentFile())
 	DrawButtons(win)
 	DrawImageCount(win, len(stateInfo.files))
 }
@@ -39,17 +43,62 @@ func RenderCurrentState(win *pixelgl.Window) {
 func SetBindings() {
 	eventChannel := input.GetKeyPressEvents()
 
+	// All changes that have happened
+	previousChanges := []files.Change{}
+
+	// The current change
+	currentChange := files.Change{}
+	addPathToCopyTo := func(path string) {
+		currentChange.NewPaths = append(currentChange.NewPaths, path)
+	}
+
 	go func() {
 		for {
 			inputEvent := <-eventChannel
 			switch inputEvent {
 			case input.NextEvent:
 				fmt.Println("Next event!")
+				// Set change src to current file
+				currentChange.OriginalPath = currentFile().Path
+
+				// Get execute change commands
+				cmds := files.GetChangeCommands(currentChange)
+				files.ExecuteChangeCommands(cmds)
+
+				// Add change to list of prev changes, reset curr change
+				previousChanges = append(previousChanges, currentChange)
+				currentChange = files.Change{}
+
+				// Update the new info
 				getNewInfo()
+			case input.UndoEvent:
+				fmt.Println("UNDO EVENT!!!")
+				// If current change isn't empty, just reset it and exit
+				if len(currentChange.NewPaths) != 0 {
+					currentChange = files.Change{}
+					break
+				}
+
+				// If empty, exit
+				if len(previousChanges) == 0 {
+					break
+				}
+
+				// If normal, get prev change, pop it, and undo it
+				changeToUndo := previousChanges[len(previousChanges)-1]
+				previousChanges = previousChanges[:len(previousChanges)-1]
+
+				cmds := files.GetReverseChangeCommands(changeToUndo)
+				files.ExecuteChangeCommands(cmds)
+
 			case input.APressEvent:
 				fmt.Println("A pressed!")
+				addPathToCopyTo(aOutputPath)
+
 			case input.DPressEvent:
 				fmt.Println("D pressed!")
+				addPathToCopyTo(dOutputPath)
+
 			default:
 				fmt.Printf("Unhandled event: %s", inputEvent)
 			}
